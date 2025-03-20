f493957f7b75a16bcb9f297958f17bc9cfa4ca9c
huanhuan0822 <raoxuanlong8888@163.com>
2025-03-20 17:37:53 +0800

    add new pages: cor corhome skv skvhome (not complete)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Home from '@/pages/index/index.vue'
 import Login from '@/pages/login/login.vue'
 import Expert from '@/pages/expert/expert.vue'
 import ExpertVideo from '@/pages/expert/expertVideo.vue'
+import Cor from '@/pages/cor/cor.vue'
+import Corhome from '@/pages/corhome/corhome.vue'
+import Skv from '@/pages/skv/skv.vue'
+import Skvhome from '@/pages/skvhome/skvhome.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,7 +31,22 @@ const router = createRouter({
     {
       path: '/expertVideo',
       component: ExpertVideo,
+
+      path: '/cor',
+      component: Cor,
     },
+    {
+      path: '/corhome',
+      component: Corhome,
+    },
+    {
+      path: '/skv',
+      component: Skv,
+    },
+    {
+      path: '/skvhome',
+      component: Skvhome,
+    }
   ],
 })
 
